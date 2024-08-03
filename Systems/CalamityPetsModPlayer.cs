8ab33d308551646eb52e3589d59480d1d7e91aa5
Pearlie <bugrapearls@gmail.com>
2024-08-03 23:34:58 +0300

    Fixed Chalice & Possessed Skull conflict

---
+++ Systems/CalamityPetsModPlayer.cs
@@ -0,0 +1,19 @@
+﻿using CalamityMod.CalPlayer;
+using PetsOverhaul.Systems;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PetsOverhaulCalamityAddon.Systems
+{
+    public class CalamityPetsModPlayer : ModPlayer
+    {
+        public override void UpdateEquips()
+        {
+            CalamityPlayer calamityPlayer = Player.GetModPlayer<CalamityPlayer>();
+            if (Player.miscEquips[0].type == ItemID.SkeletronPetItem && calamityPlayer.chaliceOfTheBloodGod)
+            {
+                calamityPlayer.chaliceOfTheBloodGod = false;
+            }
+        }
+    }
+}
---
+++ Systems/TooltipChanges.cs
@@ -0,0 +1,23 @@
+﻿using CalamityMod.Items.Accessories;
+using PetsOverhaul.Config;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+namespace PetsOverhaul.Systems
+{
+    public class ChaliceSkeletronInteraction : GlobalItem
+    {
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            return entity.type == ModContent.ItemType<ChaliceOfTheBloodGod>();
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.Misc.ChaliceOfTheBloodGod")));
+        }
+    }
+}

3fd6886f4b25d74719d263d40c680effd2ee3b9f
Pearlie <bugrapearls@gmail.com>
2024-09-24 11:50:43 +0300

    Implemented Fox Pet

--- CalamityPets/Fox.cs
+++ CalamityPets/Fox.cs
@@ -5,15 +5,56 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.GameInput;
+using Terraria.ID;
+using System;
+using Microsoft.Xna.Framework;
 
 namespace PetsOverhaulCalamityAddon.CalamityPets
 {
     public sealed class FoxEffect : PetEffect
     {
-        public override PetClasses PetClassPrimary => PetClasses.None;
-        public override void PostUpdateEquips()
+        public override PetClasses PetClassPrimary => PetClasses.Utility;
+        public int baseTime = 120;
+        public int perTime = 30;
+        public int cooldown = 7200;
+        private int cleansePenalty = 0;
+        public override void PreUpdate()
         {
-
+            if (Pet.PetInUse(CalamityPetIDs.Fox))
+            {
+                Pet.SetPetAbilityTimer(cooldown);
+            }
+            if (cleansePenalty > 0)
+            {
+                cleansePenalty--;
+                Player.buffImmune[BuffID.Cursed] = false;
+                Player.AddBuff(BuffID.Cursed, 1);
+            }
+        }
+        public override void ProcessTriggers(TriggersSet triggersSet)
+        {
+            if (Pet.AbilityPressCheck() && Pet.PetInUseWithSwapCd(CalamityPetIDs.Fox))
+            {
+                int amountOfDebuffs = 0;
+                for (int i = 0; i < Player.MaxBuffs; i++)
+                {
+                    if (Main.debuff[Player.buffType[i]])
+                    {
+                        Player.DelBuff(i);
+                        amountOfDebuffs++;
+                    }
+                }
+                if (amountOfDebuffs > 0)
+                {
+                    if (ModContent.GetInstance<Personalization>().AbilitySoundDisabled == false)
+                        SoundEngine.PlaySound(SoundID.Item35 with { Pitch = 0.75f, PitchVariance = 0.1f }, Player.position);
+                    cleansePenalty = baseTime + perTime * (amountOfDebuffs - 1);
+                    CombatText.NewText(Player.getRect(), Color.Orange, Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.PetTooltips.FoxCleansedText") + amountOfDebuffs.ToString(),dramatic: true);
+                    Pet.timer = Pet.timerMax;
+                }
+            }
         }
     }
     public sealed class FoxDriveTooltip : GlobalItem
@@ -32,7 +73,11 @@ namespace PetsOverhaulCalamityAddon.CalamityPets
 
             FoxEffect fox = Main.LocalPlayer.GetModPlayer<FoxEffect>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.PetTooltips.FoxDrive")
-                .Replace("<class>", PetColors.ClassText(fox.PetClassPrimary, fox.PetClassSecondary))
+                .Replace("<class>", PetTextsColors.ClassText(fox.PetClassPrimary, fox.PetClassSecondary))
+                .Replace("<keybind>", PetTextsColors.KeybindText(Keybinds.UsePetAbility))
+                .Replace("<baseCurse>", Math.Round(fox.baseTime / 60f, 2).ToString())
+                .Replace("<perCurse>", Math.Round(fox.perTime / 60f, 2).ToString())
+                .Replace("<cooldown>", Math.Round(fox.cooldown / 60f, 2).ToString())
             ));
         }
     }

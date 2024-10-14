af3fe254389d9b237c29de181511c4384b99adc6
Pearlie <bugrapearls@gmail.com>
2024-10-14 21:46:54 +0300

    Completed Brimling Pet

--- CalamityPets/Brimling.cs
+++ CalamityPets/Brimling.cs
@@ -5,15 +5,76 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using CalamityMod.Projectiles.Ranged;
+using CalamityMod.Projectiles.Typeless;
+using CalamityMod.Projectiles.Summon;
+using PetsOverhaul.Projectiles;
+using Terraria.ID;
+using System;
 
 namespace PetsOverhaulCalamityAddon.CalamityPets
 {
     public sealed class BrimlingEffect : PetEffect
     {
-        public override PetClasses PetClassPrimary => PetClasses.None;
+        public override PetClasses PetClassPrimary => PetClasses.Defensive;
+        public override PetClasses PetClassSecondary => PetClasses.Offensive;
+        public float drTreshold = 0.25f;
+        public float dr = 0.15f;
+        public int baseDmg = 30;
+        public int cooldown = 210;
+        public float reflectAmount = 0.45f;
+        public override void PreUpdate()
+        {
+            if (Pet.PetInUse(CalamityPetIDs.Brimling))
+            {
+                Pet.SetPetAbilityTimer(cooldown);
+            }
+        }
         public override void PostUpdateMiscEffects()
         {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.Brimling) && Player.statLife < Player.statLifeMax2 * drTreshold)
+            {
+                Player.endurance += dr;
+            }
+        }
+        public override void OnHurt(Player.HurtInfo info)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.Brimling))
+            {
+                if (Pet.timer <= 0)
+                {
+                    float dmg = (info.SourceDamage / 2 + Player.statDefense) * (1f + Player.endurance);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Projectile proj = Projectile.NewProjectileDirect(GlobalPet.GetSource_Pet(EntitySourcePetIDs.PetProjectile), Player.Center, 2f * Main.rand.NextVector2Circular(3f, 3f), ModContent.ProjectileType<BrimstoneFireballMinion>(), Main.DamageVar(dmg, Player.luck), 0f, Player.whoAmI);
+                        proj.tileCollide = false;
+                        proj.DamageType = DamageClass.Generic;
+                        proj.netUpdate = true;
+                    }
+                    Pet.timer = Pet.timerMax;
+                }
 
+                if (info.DamageSource.TryGetCausingEntity(out Entity entity))
+                {
+                    NPC.HitInfo hit = new NPC.HitInfo() with { Crit = false, DamageType = DamageClass.Generic, HitDirection = info.HitDirection, Knockback = 0.5f };
+                    int damageTaken = info.SourceDamage > Player.statLife ? Player.statLife : info.SourceDamage; //Caps the Reflect's base damage to Player's current HP.
+                    int mult = (Player.statLife < Player.statLifeMax2 * drTreshold) ? 2 : 1;
+
+                    if (entity is Projectile projectile && projectile.TryGetGlobalProjectile(out ProjectileSourceChecks proj))
+                    {
+                        NPC npc = Main.npc[proj.sourceNpcId];
+                        npc.StrikeNPC(hit with { Damage = (int)(damageTaken * reflectAmount * mult) });
+                        if (Main.netMode != NetmodeID.SinglePlayer)
+                            NetMessage.SendStrikeNPC(npc, hit);
+                    }
+                    else if (entity is NPC npc && npc.active == true && npc.immortal == false)
+                    {
+                        npc.StrikeNPC(hit with { Damage = (int)(damageTaken * reflectAmount * mult) });
+                        if (Main.netMode != NetmodeID.SinglePlayer)
+                            NetMessage.SendStrikeNPC(npc, hit);
+                    }
+                }
+            }
         }
     }
     public sealed class CharredRelicTooltip : GlobalItem
@@ -33,6 +94,10 @@ namespace PetsOverhaulCalamityAddon.CalamityPets
             BrimlingEffect brimling = Main.LocalPlayer.GetModPlayer<BrimlingEffect>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.PetTooltips.CharredRelic")
                 .Replace("<class>", PetTextsColors.ClassText(brimling.PetClassPrimary, brimling.PetClassSecondary))
+                .Replace("<treshold>", Math.Round(brimling.drTreshold * 100, 2).ToString())
+                .Replace("<dr>", Math.Round(brimling.dr * 100, 2).ToString())
+                .Replace("<reflectAmount>", Math.Round(brimling.reflectAmount * 100, 2).ToString())
+                .Replace("<cooldown>", Math.Round(brimling.cooldown / 60f, 2).ToString())
             ));
         }
     }

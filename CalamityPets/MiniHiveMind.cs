65e2624a2373c225db2e515579e511aaadbfafd2
Pearlie <bugrapearls@gmail.com>
2024-10-15 19:07:30 +0300

    Completed Mini Hive Mind and Mini Perforator Pets

--- CalamityPets/MiniHiveMind.cs
+++ CalamityPets/MiniHiveMind.cs
@@ -5,15 +5,158 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using PetsOverhaul.Projectiles;
+using System;
+using Terraria.ModLoader.IO;
 
 namespace PetsOverhaulCalamityAddon.CalamityPets
 {
     public sealed class MiniHiveMindEffect : PetEffect
     {
-        public override PetClasses PetClassPrimary => PetClasses.None;
+        public override PetClasses PetClassPrimary => PetClasses.Offensive;
+        public int evilKills = 0;
+        public int Level = 0;
+        public List<int> expTresholds = [0, 10, 50, 150, 400, 900, 1700, 3000, 5000, 10000];
+        public const int maxLvl = 9;
+        public float damage = 0;
+        public float crit = 0;
+        public float luckVal = 0;
+        public float pen = 0;
+        public float critDmg = 0;
+        public float evilMult = 1.5f;
+        public float dmgIncrIfCorrupt = 0.06f;
+        public override void ResetEffects()
+        {
+            damage = 0;
+            crit = 0;
+            luckVal = 0;
+            pen = 0;
+            critDmg = 0;
+        }
         public override void PostUpdateMiscEffects()
         {
+            if (evilKills < 0)
+                evilKills = 0;
 
+            if (evilKills >= expTresholds[maxLvl])
+                Level = maxLvl;
+            else
+                Level = expTresholds.FindIndex(x => x > Math.Clamp(x, 0, evilKills)) - 1;
+
+            if (Level < 0)
+                Level = 0;
+
+            if (Level >= 0)
+            {
+                damage += 0.03f;
+                crit += 1f;
+            }
+            if (Level >= 1)
+            {
+                damage += 0.01f;
+            }
+            if (Level >= 2)
+            {
+                luckVal += 0.005f;
+                damage += 0.01f;
+            }
+            if (Level >= 3)
+            {
+                damage += 0.01f;
+                crit += 1f;
+                pen += 1f;
+            }
+            if (Level >= 4)
+            {
+                critDmg += 0.02f;
+                damage += 0.02f;
+            }
+            if (Level >= 5)
+            {
+                luckVal += 0.005f;
+                crit += 1f;
+                pen += 1f;
+                damage += 0.02f;
+            }
+            if (Level >= 6)
+            {
+                damage += 0.02f;
+                crit += 1f;
+                pen += 2f;
+            }
+            if (Level >= 7)
+            {
+                damage += 0.01f;
+                pen += 2f;
+                luckVal += 0.01f;
+                critDmg += 0.03f;
+            }
+            if (Level >= 8)
+            {
+                crit += 2f;
+                damage += 0.02f;
+                pen += 1f;
+                luckVal += 0.02f;
+            }
+            if (Level >= 9)
+            {
+                crit += 1f;
+                damage += 0.01f;
+                pen += 2f;
+                luckVal += 0.05f;
+                critDmg += 0.01f;
+            }
+            if (Player.ZoneCorrupt)
+            {
+                crit *= evilMult;
+                damage *= evilMult;
+                pen *= evilMult;
+                luckVal *= evilMult;
+                critDmg *= evilMult;
+            }
+
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniHiveMind))
+            {
+                Player.GetCritChance<GenericDamageClass>() += crit;
+                Player.GetDamage<GenericDamageClass>() += damage;
+                Player.GetArmorPenetration<GenericDamageClass>() += pen;
+            }
+        }
+        public override void ModifyLuck(ref float luck)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniHiveMind))
+            {
+                luck += luckVal;
+            }
+        }
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniHiveMind))
+            {
+                modifiers.CritDamage += critDmg;
+                if (GlobalPet.CrimsonEnemies.Contains(target.type))
+                {
+                    modifiers.FinalDamage *= 1f + dmgIncrIfCorrupt;
+                }
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniHiveMind) && target.active == false && GlobalPet.CrimsonEnemies.Contains(target.type))
+            {
+                evilKills++;
+            }
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("HiveKills", evilKills);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.TryGet("HiveKills", out int kills))
+            {
+                evilKills = kills;
+            }
         }
     }
     public sealed class RottingEyeballTooltip : GlobalItem
@@ -25,16 +168,23 @@ namespace PetsOverhaulCalamityAddon.CalamityPets
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
-            if (ModContent.GetInstance<Personalization>().EnableTooltipToggle && !Keybinds.PetTooltipHide.Current)
+            if (ModContent.GetInstance<PetPersonalization>().EnableTooltipToggle && !PetKeybinds.PetTooltipHide.Current)
             {
                 return;
             }
 
-
-
             MiniHiveMindEffect hive = Main.LocalPlayer.GetModPlayer<MiniHiveMindEffect>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.PetTooltips.RottingEyeball")
-                    .Replace("<class>", PetTextsColors.ClassText(hive.PetClassPrimary, hive.PetClassSecondary))
+                        .Replace("<class>", PetTextsColors.ClassText(hive.PetClassPrimary, hive.PetClassSecondary))
+                        .Replace("<incrToCorrupt>", Math.Round(hive.dmgIncrIfCorrupt * 100, 2).ToString())
+                        .Replace("<killCount>", hive.evilKills.ToString())
+                        .Replace("<dmg>", Math.Round(hive.damage * 100, 2).ToString())
+                        .Replace("<crit>", hive.crit.ToString())
+                        .Replace("<luck>", hive.luckVal.ToString())
+                        .Replace("<pen>", hive.pen.ToString())
+                        .Replace("<critDmg>", Math.Round(hive.critDmg * 100, 2).ToString())
+                        .Replace("<evilMult>", hive.evilMult.ToString())
+                        .Replace("<killReq>", hive.Level >= MiniHiveMindEffect.maxLvl ? Language.GetTextValue("Mods.PetsOverhaul.PetItemTooltips.JunimoMaxed") : (hive.expTresholds[Math.Clamp(hive.Level + 1, 0, MiniHiveMindEffect.maxLvl)] - hive.evilKills).ToString())
                 ));
         }
     }
--- CalamityPets/MiniPerforator.cs
+++ CalamityPets/MiniPerforator.cs
@@ -5,15 +5,167 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using System;
+using Terraria.ID;
+using log4net.Core;
+using CalamityMod.NPCs;
+using Terraria.Utilities;
+using CalamityMod;
+using PetsOverhaul.Projectiles;
+using CalamityMod.Projectiles.Summon;
 
 namespace PetsOverhaulCalamityAddon.CalamityPets
 {
     public sealed class MiniPerforatorEffect : PetEffect
     {
-        public override PetClasses PetClassPrimary => PetClasses.None;
+        public override PetClasses PetClassPrimary => PetClasses.Defensive;
+        public int evilKills = 0;
+        public int Level = 0;
+        public List<int> expTresholds = [0, 10, 50, 150, 400, 900, 1700, 3000, 5000, 10000];
+        public const int maxLvl = 9;
+        public int defense = 0;
+        public int health = 0;
+        public float luckVal = 0;
+        public float dr = 0;
+        public int regen = 0;
+        public float evilMult = 1.5f;
+        public float drIfHurtByCrimson = 0.06f;
+        public override void ResetEffects()
+        {
+            defense = 0;
+            health = 0;
+            luckVal = 0;
+            dr = 0;
+            regen = 0;
+        }
         public override void PostUpdateMiscEffects()
         {
+            if (evilKills < 0)
+                evilKills = 0;
 
+            if (evilKills >= expTresholds[maxLvl])
+                Level = maxLvl;
+            else
+                Level = expTresholds.FindIndex(x => x > Math.Clamp(x, 0, evilKills)) - 1;
+
+            if (Level < 0)
+                Level = 0;
+
+            if (Level >= 0)
+            {
+                health += 5;
+                defense += 2;
+            }
+            if (Level >= 1)
+            {
+                health += 5;
+            }
+            if (Level >= 2)
+            {
+                luckVal += 0.005f;
+                health += 5;
+            }
+            if (Level >= 3)
+            {
+                defense += 1;
+                health += 5;
+                dr += 0.01f;
+            }
+            if (Level >= 4)
+            {
+                regen += 1;
+                health += 5;
+            }
+            if (Level >= 5)
+            {
+                luckVal += 0.005f;
+                health += 5;
+                dr += 0.01f;
+                defense += 1;
+            }
+            if (Level >= 6)
+            {
+                defense += 2;
+                health += 5;
+                dr += 0.01f;
+            }
+            if (Level >= 7)
+            {
+                health += 5;
+                dr += 0.01f;
+                luckVal += 0.01f;
+                regen += 1;
+            }
+            if (Level >= 8)
+            {
+                health += 5;
+                defense += 2;
+                dr += 0.02f;
+                luckVal += 0.02f;
+            }
+            if (Level >= 9)
+            {
+                health += 5;
+                defense += 1;
+                dr += 0.01f;
+                luckVal += 0.05f;
+                regen += 1;
+            }
+            if (Player.ZoneCrimson)
+            {
+                health = (int)(health * evilMult);
+                defense = (int)(defense * evilMult);
+                dr *= evilMult;
+                luckVal *= evilMult;
+                regen = (int)(regen * evilMult);
+            }
+
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniPerforator))
+            {
+                Player.statLifeMax2 += health;
+                Player.statDefense += defense;
+                Player.endurance += dr;
+            }
+        }
+        public override void UpdateLifeRegen()
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniPerforator))
+            {
+                Player.lifeRegen += regen;
+            }
+        }
+        public override void ModifyLuck(ref float luck)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniPerforator))
+            {
+                luck += luckVal;
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniPerforator) && target.active == false && GlobalPet.CrimsonEnemies.Contains(target.type))
+            {
+                evilKills++;
+            }
+        }
+        public override void ModifyHurt(ref Player.HurtModifiers modifiers)
+        {
+            if (Pet.PetInUseWithSwapCd(CalamityPetIDs.MiniPerforator) && modifiers.DamageSource.TryGetCausingEntity(out Entity entity) && ((entity is Projectile proj && proj.TryGetGlobalProjectile(out ProjectileSourceChecks source) && GlobalPet.CrimsonEnemies.Contains(Main.npc[source.sourceNpcId].type)) || (entity is NPC npc && GlobalPet.CrimsonEnemies.Contains(npc.type))))
+            {
+                modifiers.FinalDamage *= 1f - drIfHurtByCrimson;
+            }
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("PerforatorKills", evilKills);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.TryGet("PerforatorKills", out int kills))
+            {
+                evilKills = kills;
+            }
         }
     }
     public sealed class BloodyVeinTooltip : GlobalItem
@@ -25,15 +177,24 @@ namespace PetsOverhaulCalamityAddon.CalamityPets
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
-            if (ModContent.GetInstance<Personalization>().EnableTooltipToggle && !Keybinds.PetTooltipHide.Current)
+            if (ModContent.GetInstance<PetPersonalization>().EnableTooltipToggle && !PetKeybinds.PetTooltipHide.Current)
             {
                 return;
             }
 
             MiniPerforatorEffect perforator = Main.LocalPlayer.GetModPlayer<MiniPerforatorEffect>();
             tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.PetTooltips.BloodyVein")
-                    .Replace("<class>", PetTextsColors.ClassText(perforator.PetClassPrimary, perforator.PetClassSecondary))
-                ));
+                        .Replace("<class>", PetTextsColors.ClassText(perforator.PetClassPrimary, perforator.PetClassSecondary))
+                        .Replace("<drFromCrimson>", Math.Round(perforator.drIfHurtByCrimson * 100, 2).ToString())
+                        .Replace("<killCount>", perforator.evilKills.ToString())
+                        .Replace("<def>", perforator.defense.ToString())
+                        .Replace("<hp>", perforator.health.ToString())
+                        .Replace("<luck>", perforator.luckVal.ToString())
+                        .Replace("<dr>", Math.Round(perforator.dr * 100, 2).ToString())
+                        .Replace("<regen>", perforator.regen.ToString())
+                        .Replace("<evilMult>", perforator.evilMult.ToString())
+                        .Replace("<killReq>", perforator.Level >= MiniPerforatorEffect.maxLvl ? Language.GetTextValue("Mods.PetsOverhaul.PetItemTooltips.JunimoMaxed") : (perforator.expTresholds[Math.Clamp(perforator.Level + 1, 0, MiniPerforatorEffect.maxLvl)] - perforator.evilKills).ToString())
+                    ));
         }
     }
 }

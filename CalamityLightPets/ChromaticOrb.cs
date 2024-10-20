7aa4e19cab6c83a8a6a353c0e39152ec6f860535
Pearlie <bugrapearls@gmail.com>
2024-10-20 21:25:23 +0300

    Completed Light Pets

---
+++ CalamityLightPets/ChromaticOrb.cs
@@ -0,0 +1,102 @@
+﻿using CalamityMod;
+using CalamityMod.Balancing;
+using PetsOverhaul.Config;
+using PetsOverhaul.Systems;
+using PetsOverhaulCalamityAddon.Systems;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace PetsOverhaul.LightPets
+{
+    public sealed class ChromaticOrbEffect : LightPetEffect
+    {
+        public override void PostUpdateMiscEffects()
+        {
+            if (Player.miscEquips[1].TryGetGlobalItem(out ChromaticOrbPet orb))
+            {
+                Pet.abilityHaste += orb.AbilityHaste.CurrentStatFloat;
+                Player.GetCritChance<GenericDamageClass>() += orb.CritChance.CurrentStatFloat * 100;
+                Player.jumpSpeedBoost += orb.JumpSpeed.CurrentStatFloat;
+            }
+        }
+    }
+    public sealed class ChromaticOrbPet : GlobalItem
+    {
+        public LightPetStat AbilityHaste = new(40, 0.0045f, 0.07f);
+        public LightPetStat CritChance = new(30, 0.0025f, 0.03f);
+        public LightPetStat JumpSpeed = new(10, 0.015f, 0.05f);
+        //Thought about adding bonus Rage damage, but could be too much for 1 pet regarding damage output. Maybe in future if seems ok.
+        public override bool InstancePerEntity => true;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            return entity.type == CalamityLightPetIDs.Yuu;
+        }
+        public override void UpdateInventory(Item item, Player player)
+        {
+            AbilityHaste.SetRoll();
+            CritChance.SetRoll();
+            JumpSpeed.SetRoll();
+        }
+        public override void NetSend(Item item, BinaryWriter writer)
+        {
+            writer.Write((byte)AbilityHaste.CurrentRoll);
+            writer.Write((byte)CritChance.CurrentRoll);
+            writer.Write((byte)JumpSpeed.CurrentRoll);
+        }
+        public override void NetReceive(Item item, BinaryReader reader)
+        {
+            AbilityHaste.CurrentRoll = reader.ReadByte();
+            CritChance.CurrentRoll = reader.ReadByte();
+            JumpSpeed.CurrentRoll = reader.ReadByte();
+        }
+        public override void SaveData(Item item, TagCompound tag)
+        {
+            tag.Add("Stat1", AbilityHaste.CurrentRoll);
+            tag.Add("Stat2", CritChance.CurrentRoll);
+            tag.Add("Stat3", JumpSpeed.CurrentRoll);
+        }
+        public override void LoadData(Item item, TagCompound tag)
+        {
+            if (tag.TryGet("Stat1", out int haste))
+            {
+                AbilityHaste.CurrentRoll = haste;
+            }
+
+            if (tag.TryGet("Stat2", out int crit))
+            {
+                CritChance.CurrentRoll = crit;
+            }
+
+            if (tag.TryGet("Stat3", out int jump))
+            {
+                JumpSpeed.CurrentRoll = jump;
+            }
+        }
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (ModContent.GetInstance<PetPersonalization>().EnableTooltipToggle && !PetKeybinds.PetTooltipHide.Current)
+            {
+                return;
+            }
+            tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.LightPetTooltips.ChromaticOrb")
+
+                        .Replace("<haste>", AbilityHaste.BaseAndPerQuality())
+                        .Replace("<crit>", CritChance.BaseAndPerQuality())
+                        .Replace("<jump>", JumpSpeed.BaseAndPerQuality())
+
+                        .Replace("<hasteLine>", AbilityHaste.StatSummaryLine())
+                        .Replace("<critLine>", CritChance.StatSummaryLine())
+                        .Replace("<jumpLine>", JumpSpeed.StatSummaryLine())
+                        ));
+            if (CritChance.CurrentRoll <= 0)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", PetTextsColors.RollMissingText()));
+            }
+        }
+    }
+}
---
+++ CalamityLightPets/EnchantedButterfly.cs
@@ -0,0 +1,114 @@
+﻿using CalamityMod;
+using CalamityMod.Balancing;
+using PetsOverhaul.Config;
+using PetsOverhaul.Systems;
+using PetsOverhaulCalamityAddon.Systems;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace PetsOverhaul.LightPets
+{
+    public sealed class EnchantedButterflyEffect : LightPetEffect
+    {
+        public override void PostUpdateMiscEffects()
+        {
+            if (Player.miscEquips[1].TryGetGlobalItem(out EnchantedButterflyPet butter))
+            {
+                Pet.petHealMultiplier += butter.PetHealPower.CurrentStatInt;
+                Pet.globalFortune += butter.GlobalFortune.CurrentStatInt;
+                Player.aggro += butter.Aggro.CurrentStatInt;
+                Player.statManaMax2 += butter.Mana.CurrentStatInt;
+            }
+        }
+    }
+    public sealed class EnchantedButterflyPet : GlobalItem
+    {
+        public LightPetStat PetHealPower = new(30, 10, 50);
+        public LightPetStat GlobalFortune = new(16, 1, 6);
+        public LightPetStat Aggro = new(20, -5, -40);
+        public LightPetStat Mana = new(10, 4, 25);
+        public override bool InstancePerEntity => true;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            return entity.type == CalamityLightPetIDs.Sparks;
+        }
+        public override void UpdateInventory(Item item, Player player)
+        {
+            PetHealPower.SetRoll();
+            GlobalFortune.SetRoll();
+            Aggro.SetRoll();
+            Mana.SetRoll();
+        }
+        public override void NetSend(Item item, BinaryWriter writer)
+        {
+            writer.Write((byte)PetHealPower.CurrentRoll);
+            writer.Write((byte)GlobalFortune.CurrentRoll);
+            writer.Write((byte)Aggro.CurrentRoll);
+            writer.Write((byte)Mana.CurrentRoll);
+        }
+        public override void NetReceive(Item item, BinaryReader reader)
+        {
+            PetHealPower.CurrentRoll = reader.ReadByte();
+            GlobalFortune.CurrentRoll = reader.ReadByte();
+            Aggro.CurrentRoll = reader.ReadByte();
+            Mana.CurrentRoll = reader.ReadByte();
+        }
+        public override void SaveData(Item item, TagCompound tag)
+        {
+            tag.Add("Stat1", PetHealPower.CurrentRoll);
+            tag.Add("Stat2", GlobalFortune.CurrentRoll);
+            tag.Add("Stat3", Aggro.CurrentRoll);
+            tag.Add("Stat4", Mana.CurrentRoll);
+        }
+        public override void LoadData(Item item, TagCompound tag)
+        {
+            if (tag.TryGet("Stat1", out int heal))
+            {
+                PetHealPower.CurrentRoll = heal;
+            }
+
+            if (tag.TryGet("Stat2", out int fortune))
+            {
+                GlobalFortune.CurrentRoll = fortune;
+            }
+
+            if (tag.TryGet("Stat3", out int aggro))
+            {
+                Aggro.CurrentRoll = aggro;
+            }
+
+            if (tag.TryGet("Stat4", out int mana))
+            {
+                Mana.CurrentRoll = mana;
+            }
+        }
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (ModContent.GetInstance<PetPersonalization>().EnableTooltipToggle && !PetKeybinds.PetTooltipHide.Current)
+            {
+                return;
+            }
+            tooltips.Add(new(Mod, "Tooltip0", Language.GetTextValue("Mods.PetsOverhaulCalamityAddon.LightPetTooltips.EnchantedButterfly")
+
+                        .Replace("<heal>", PetHealPower.BaseAndPerQuality())
+                        .Replace("<fortune>", GlobalFortune.BaseAndPerQuality())
+                        .Replace("<aggro>", Aggro.BaseAndPerQuality())
+                        .Replace("<mana>", Mana.BaseAndPerQuality())
+
+                        .Replace("<healLine>", PetHealPower.StatSummaryLine())
+                        .Replace("<fortuneLine>", GlobalFortune.StatSummaryLine())
+                        .Replace("<aggroLine>", Aggro.StatSummaryLine(Aggro.CurrentStatInt.ToString())) //Using overload so the + doesn't appear on tooltip
+                        .Replace("<manaLine>", Mana.StatSummaryLine())
+                        ));
+            if (GlobalFortune.CurrentRoll <= 0)
+            {
+                tooltips.Add(new(Mod, "Tooltip0", PetTextsColors.RollMissingText()));
+            }
+        }
+    }
+}

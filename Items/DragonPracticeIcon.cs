3e86213aef9c472067ce1a740ee48981f28ceff0
Pearlie <bugrapearls@gmail.com>
2025-05-24 23:54:40 +0300

    Made Dragon practice icon not be able to exist on world & in inventory.

--- Items/DragonPracticeIcon.cs
+++ Items/DragonPracticeIcon.cs
@@ -1,9 +1,26 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace PetsOverhaulCalamityAddon.Items
 {
     /// <summary>
     /// Credit of Icon goes to Riot Games, taken by Smolder's Passive Icon and resized to 30x32. This Items only purpose is for Akato's tooltip to have Icons for representing Stack buffs. 
     /// </summary>
-    public class DragonPracticeIcon : ModItem { }
+    public class DragonPracticeIcon : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ItemsThatShouldNotBeInInventory[Type] = true;
+            //ItemID.Sets.Deprecated[Type] = true; //This doesn't work; since it doesn't let us see the icon of item when this is set to true.
+        }
+        public override void PostUpdate()
+        {
+            Item.TurnToAir(true);
+        }
+        public override void UpdateInventory(Player player)
+        {
+            Item.TurnToAir(true);
+        }
+    }
 }
